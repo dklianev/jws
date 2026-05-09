4f162320a9690dc8f95dde2e86558048e6d425b3
dklianev <mitko6754@gmail.com>
2026-05-09 09:12:37 +0300

    Initialize Gradle Spring Boot project

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.11"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.informatics"
+version = "0.0.1-SNAPSHOT"
+description = "Winter Olympics project for Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.modelmapper:modelmapper:3.2.1")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	annotationProcessor("org.projectlombok:lombok")
+    testImplementation("com.h2database:h2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/org/informatics/winter_olympics/WinterOlympicsApplication.java
@@ -0,0 +1,12 @@
+package org.informatics.winter_olympics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterOlympicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinterOlympicsApplication.class, args);
+    }
+}
