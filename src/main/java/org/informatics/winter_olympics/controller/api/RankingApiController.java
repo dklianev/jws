9377524b665b16f3e0fb99a8d14d4473574c414d
dklianev <mitko6754@gmail.com>
2026-05-15 10:31:36 +0300

    Expose ranking REST API

---
+++ src/main/java/org/informatics/winter_olympics/controller/api/RankingApiController.java
@@ -0,0 +1,39 @@
+package org.informatics.winter_olympics.controller.api;
+
+import lombok.AllArgsConstructor;
+import org.informatics.winter_olympics.dto.MedalCountDto;
+import org.informatics.winter_olympics.dto.OlympicsStatisticsDto;
+import org.informatics.winter_olympics.dto.RankingEntryDto;
+import org.informatics.winter_olympics.dto.SlalomResultDto;
+import org.informatics.winter_olympics.service.RankingService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rankings")
+@AllArgsConstructor
+public class RankingApiController {
+
+    private final RankingService rankingService;
+
+    @GetMapping("/competition/{competitionId}")
+    public List<RankingEntryDto> getCompetitionRanking(@PathVariable long competitionId) {
+        return rankingService.getCompetitionRanking(competitionId);
+    }
+
+    @GetMapping("/competition/{competitionId}/second-run")
+    public List<SlalomResultDto> getRunTwoStartOrder(@PathVariable long competitionId) {
+        return rankingService.getRunTwoStartOrder(competitionId);
+    }
+
+    @GetMapping("/olympics/{olympicsId}/medals")
+    public List<MedalCountDto> getMedalTable(@PathVariable long olympicsId) {
+        return rankingService.getMedalTable(olympicsId);
+    }
+
+    @GetMapping("/olympics/{olympicsId}/statistics")
+    public OlympicsStatisticsDto getOlympicsStatistics(@PathVariable long olympicsId) {
+        return rankingService.getOlympicsStatistics(olympicsId);
+    }
+}
